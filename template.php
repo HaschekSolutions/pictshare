1d94c974df3fdb4f615e5e1bbe1034c439a0dcdf
Christian Haschek <christian@haschek.at>
2015-10-27 17:47:29 +0100

    github link now left

--- template.php
+++ template.php
@@ -28,7 +28,6 @@
 
     </HEAD>
     <BODY>
-        <a href="https://github.com/chrisiaut/pictshare"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
       <div class="mitte" id="overall">
         <a href="/"><div id="header">
             <div style="padding:15px;"><div class="rechts"><?php echo $slogan; ?></div></div>
@@ -39,5 +38,7 @@
         <div class="content"><center>(c)<?php echo date("y");?> by<br/><a href="http://haschek-solutions.com" target="_blank"><img height="30" src="/css/imgs/hs_logo.png" /></a></center></div>
         <div id="footer"></div>
       </div>
+      
+      <a href="https://github.com/chrisiaut/pictshare"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/82b228a3648bf44fc1163ef44c62fcc60081495e/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png"></a>
     </BODY>
 </HTML>
