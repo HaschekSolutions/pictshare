2472d188aae7811ec72000fb5e65d6e940c377fb
Christian Haschek <christian@haschek.at>
2015-10-31 15:15:26 +0100

    fixed a bug where the original picture was shown after the max_resized_images was met but the requested image was already cached

--- inc/core.php
+++ inc/core.php
@@ -180,8 +180,7 @@ function renderImage($data)
         $path = $cachepath;
         $cached = true;
     }
-
-    if($pm->countResizedImages($hash)>=MAX_RESIZED_IMAGES) //if the number of max resized images is reached, just show the real one
+    else if($pm->countResizedImages($hash)>MAX_RESIZED_IMAGES) //if the number of max resized images is reached, just show the real one
         $path = ROOT.DS.'upload'.DS.$hash.DS.$hash;
     
     switch($type)
