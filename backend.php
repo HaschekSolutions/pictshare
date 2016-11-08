9993460583d35432dd522f771e62bbc39b6d9226
Christian Haschek <christian@haschek.at>
2016-11-08 18:37:49 +0100

    fixed backend bug. fixes #21

--- backend.php
+++ backend.php
@@ -5,7 +5,7 @@ session_cache_expire($expiry * 24 * 60);
 session_start();
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(__FILE__));
-define('PATH',((dirname($_SERVER['PHP_SELF'])=='/'||dirname($_SERVER['PHP_SELF'])=='/index.php')?'/':dirname($_SERVER['PHP_SELF']).'/'));
+define('PATH',((dirname($_SERVER['PHP_SELF'])=='/'||dirname($_SERVER['PHP_SELF'])=='/index.php'||dirname($_SERVER['PHP_SELF'])=='/backend.php')?'/':dirname($_SERVER['PHP_SELF']).'/'));
 
 if(!file_exists(ROOT.DS.'inc'.DS.'config.inc.php'))
 	exit('Rename /inc/example.config.inc.php to /inc/config.inc.php first!');
