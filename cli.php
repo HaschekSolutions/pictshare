d685c4e93521af31b75f81fbfb315b44ed570ec9
Christian <christian@haschek.at>
2016-11-29 17:25:20 +0100

    fixed bug for windows

--- cli.php
+++ cli.php
@@ -3,7 +3,7 @@
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(__FILE__));
 define('CLI', true);
-define('PATH',((dirname($_SERVER['PHP_SELF'])=='/'||dirname($_SERVER['PHP_SELF'])=='/index.php')?'/':dirname($_SERVER['PHP_SELF']).'/'));
+define('PATH',((dirname($_SERVER['PHP_SELF'])=='/'||dirname($_SERVER['PHP_SELF'])=='\\'||dirname($_SERVER['PHP_SELF'])=='/index.php'||dirname($_SERVER['PHP_SELF'])=='/backend.php')?'/':dirname($_SERVER['PHP_SELF']).'/'));
 
 if(!file_exists(ROOT.DS.'inc'.DS.'config.inc.php'))
 	exit('Rename /inc/example.config.inc.php to /inc/config.inc.php first!');
