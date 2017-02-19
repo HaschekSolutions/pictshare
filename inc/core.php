0864dcdf22f538c6a96a1ae2626026a0ae4856d0
Christian <christian@haschek.at>
2017-02-19 11:51:49 +0100

    added backwards compatibility for random generation

--- inc/core.php
+++ inc/core.php
@@ -407,7 +407,7 @@ function getRandomString($length=32, $keyspace = '0123456789abcdefghijklmnopqrst
     $str = '';
     $max = mb_strlen($keyspace, '8bit') - 1;
     for ($i = 0; $i < $length; ++$i) {
-        $str .= $keyspace[random_int(0, $max)];
+        $str .= $keyspace[rand(0, $max)];
     }
     return $str;
 }
