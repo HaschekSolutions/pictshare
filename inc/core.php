6f897d75b4e48c0a747289e6121118765c589b3b
Christian <christian@haschek.at>
2017-02-19 11:48:29 +0100

    added individual delete codes fixes #24

--- inc/core.php
+++ inc/core.php
@@ -401,3 +401,13 @@ function serveFile($filename, $filename_output = false, $mime = 'application/oct
         flush();
     }
 }
+
+function getRandomString($length=32, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyz')
+{
+    $str = '';
+    $max = mb_strlen($keyspace, '8bit') - 1;
+    for ($i = 0; $i < $length; ++$i) {
+        $str .= $keyspace[random_int(0, $max)];
+    }
+    return $str;
+}
