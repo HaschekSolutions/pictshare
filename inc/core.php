12073c0e027d157c271c8964631e742247abebea
Christian <christian@haschek.at>
2017-02-19 10:07:42 +0100

    function to get user ip even with reverse proxy

--- inc/core.php
+++ inc/core.php
@@ -14,13 +14,19 @@ function getUserIP()
 	$client  = @$_SERVER['HTTP_CLIENT_IP'];
 	$forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
 	$remote  = $_SERVER['REMOTE_ADDR'];
-	if(filter_var($client, FILTER_VALIDATE_IP))
-	{
-		$ip = $client;
-	}
-	elseif(filter_var($forward, FILTER_VALIDATE_IP))
+	
+    if(strpos($forward,','))
+    {
+        $a = explode(',',$forward);
+        $forward = trim($a[0]);
+    }
+	if(filter_var($forward, FILTER_VALIDATE_IP))
 	{
 		$ip = $forward;
+	}
+    elseif(filter_var($client, FILTER_VALIDATE_IP))
+	{
+		$ip = $client;
 	}
 	else
 	{
