ca0110737906bd1e1d9a1d5d85bfd7c1e0dc0c5d
Chris <christian@haschek.at>
2025-05-17 21:27:20 +0200

    cleanup

--- src/lib/vendor/aws/aws-sdk-php/src/data/acm/2015-12-08/paginators-1.json.php
+++
@@ -1,3 +0,0 @@
-<?php
-// This file was auto-generated from sdk-root/src/data/acm/2015-12-08/paginators-1.json
-return [ 'pagination' => [ 'ListCertificates' => [ 'input_token' => 'NextToken', 'output_token' => 'NextToken', 'limit_key' => 'MaxItems', 'result_key' => 'CertificateSummaryList', ], ],];
--- src/lib/vendor/bitverse/identicon/tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$loader = __DIR__.'/../vendor/autoload.php';
-
-if (!file_exists($loader)) {
-    throw new RuntimeException('Install dependencies to run test suite.');
-}
-
-require_once $loader;
